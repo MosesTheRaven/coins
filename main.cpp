067633566bc68ea03502bb348430650afc6b728f
JimCarry <dajo.lazar@gmail.com>
2016-11-08 20:19:59 +0100

    first major commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+void  helloAgain(){
+    cout << "hello again";
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    helloAgain();
+
     return 0;
 }
