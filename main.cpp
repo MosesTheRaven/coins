c0845947ebbced02e78b14933e0f741fb7d5b49d
JimCarry <dajo.lazar@gmail.com>
2016-11-09 08:11:33 +0100

    first major commit

--- main.cpp
+++ main.cpp
@@ -2,25 +2,9 @@
 
 using namespace std;
 
-void  helloAgain(){
-    cout << "hello again";
-}
-
-
-void  helloAgainAgain(){
-    cout << "hello again again";
-}
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    helloAgain();
 
     return 0;
-    //Nejake ine zmeny
-}
-void vyvpisNieco(){
-    //som vedel programovat v c, dal by som tu vypis do konzoly\
-
-
 }
