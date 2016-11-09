d3b1e1a30174f8d912d7aad5a5157f361f8738b0
JimCarry <dajo.lazar@gmail.com>
2016-11-09 08:18:16 +0100

    skusanie nacitania vstupu

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+
+struct coin{
+    int coinValue;
+    int coinCount;
+};
+
+int CoinsCount, Amount;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    cin >> CoinsCount >> Amount;
+    // vytvorime pole
+    coin * Coins = new coin[CoinsCount];
+    //nacitame hodnoty
+    for (int i = 0; i < CoinsCount; i++) {
+        cin >> Coins[i].coinValue >> Coins[i].coinCount;
+    }
+    for (int i = 0; i < CoinsCount; ++i) {
+        cout << Coins[i].coinValue << " x " << Coins[i].coinCount;
+    }
 
     return 0;
 }
