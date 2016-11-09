763e3fc8fd397bec5fe315e586601f62f9403b00
JimCarry <dajo.lazar@gmail.com>
2016-11-09 09:03:08 +0100

    quicksort minci

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
-
 
 struct coin{
     int coinValue;
@@ -9,6 +9,39 @@ struct coin{
 };
 
 int CoinsCount, Amount;
+
+void quicksort(coin  array[], int left, int right){
+    if(left < right){
+        int boundary = left;
+        for(int i = left + 1; i < right; i++){
+            if(array[i].coinValue > array[left].coinValue){
+                int swapLeft = i;
+                int swapRight = ++boundary;
+
+                int tmpCount = array[swapRight].coinCount;
+                int tmpValue = array[swapRight].coinValue;
+
+                array[swapRight].coinCount = array[swapLeft].coinCount;
+                array[swapRight].coinValue= array[swapLeft].coinValue;
+                array[swapLeft].coinCount = tmpCount;
+                array[swapLeft].coinValue = tmpValue;
+            }
+        }
+        int swapLeft = left;
+        int swapRight = boundary;
+
+        int tmpCount = array[swapRight].coinCount;
+        int tmpValue = array[swapRight].coinValue;
+
+        array[swapRight].coinCount = array[swapLeft].coinCount;
+        array[swapRight].coinValue= array[swapLeft].coinValue;
+        array[swapLeft].coinCount = tmpCount;
+        array[swapLeft].coinValue = tmpValue;
+
+        quicksort(array, left, boundary);
+        quicksort(array, boundary + 1, right);
+    }
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -20,8 +53,12 @@ int main() {
     for (int i = 0; i < CoinsCount; i++) {
         cin >> Coins[i].coinValue >> Coins[i].coinCount;
     }
+    cout << "Zotriedene pole minci" << endl;
+
+    quicksort(Coins, 0, CoinsCount);
+
     for (int i = 0; i < CoinsCount; ++i) {
-        cout << Coins[i].coinValue << " x " << Coins[i].coinCount;
+        cout << Coins[i].coinValue << "x" << Coins[i].coinCount << endl;
     }
 
     return 0;
