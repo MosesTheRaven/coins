f7e5bbc770d11c9baa0a80411ce8946ac33e8fc4
JimCarry <dajo.lazar@gmail.com>
2016-11-10 16:23:48 +0100

    hopefully clean & with comments

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
+//komentar k rieseniu a premennym je pri int main() nizsie
+
 #include <iostream>
-#include <array>
 
 using namespace std;
 
-int CoinsTypeCount, Amount, returnCoinsAmount, bestCoinsAmount ;
+int CoinsTypeCount, Amount, returnCoinsAmount;
 bool foundSolution;
 int * coinValue, * coinCount,  *returnValue, * returnCount ;
+//globalne pre ulahcnenie prace
+//pri pouzivani smernikov som (dufam) zmazal vsetko, s cim som uz dalej nemal v plane pracovat
 
 int getIndex(int searchedNumber){
+    //funkcia, ktora vrati index, na ktorom sa nachadza hladane cislo
+    //potrebne kvoli quicksortu, ktore pomeni poradie minci
     int retVal = 0;
     for(retVal; retVal < CoinsTypeCount; retVal++){
         if (coinValue[retVal] == searchedNumber) break;
@@ -16,16 +21,15 @@ int getIndex(int searchedNumber){
 }
 
 void quicksort(int left, int right){
+    //quicksort na zotriedenie minci podla velkosti - toto vyrazne urychli celkovy vypocet
     if(left < right){
         int boundary = left;
         for(int i = left + 1; i < right; i++){
             if(coinValue[i] > coinValue[left]){
                 int swapLeft = i;
                 int swapRight = ++boundary;
-
                 int tmpCount = coinCount[swapRight];
                 int tmpValue = coinValue[swapRight];
-
                 coinCount[swapRight] = coinCount[swapLeft];
                 coinValue[swapRight]= coinValue[swapLeft];
                 coinCount[swapLeft] = tmpCount;
@@ -34,24 +38,22 @@ void quicksort(int left, int right){
         }
         int swapLeft = left;
         int swapRight = boundary;
-
         int tmpCount = coinCount[swapRight];
         int tmpValue = coinValue[swapRight];
-
         coinCount[swapRight] = coinCount[swapLeft];
         coinValue[swapRight] = coinValue[swapLeft];
         coinCount[swapLeft] = tmpCount;
         coinValue [swapLeft]= tmpValue;
-
         quicksort(left, boundary);
         quicksort(boundary + 1, right);
     }
 }
 
 void payout(int * usedCoins, int remainingSum, int ignoreIndex){
-    //cout << "spustila sa rekurzia so sumou" << remainingSum << endl;
+    //rekurzia, ktora odcitava od sumy mince, pricom ignoruje tie, ktore uz boli odcitane
     //koncime vetvu rekurzie, ak sme dosiahli sumu, ktoru sme mali vyplatit
     if (remainingSum == 0) {
+        //nasli sme nejake riesenie
         foundSolution = true;
         //skontrolujeme, ci mame najlepsie riesenie
         int usedCoinsAmount = 0;
@@ -64,9 +66,9 @@ void payout(int * usedCoins, int remainingSum, int ignoreIndex){
             for (int i = 0; i < CoinsTypeCount; ++i) {
                 returnCount[i] = coinCount[i] - usedCoins[i];
             }
-            bestCoinsAmount = returnCoinsAmount;
         }
-        //inak nechame tak, pretoze sme predtym nasli lepsie riesenie
+        //inak zahodime tak, pretoze sme predtym nasli lepsie riesenie
+        delete(usedCoins);
     }
     //inak sa pokracujeme v rekurzii
     //pripad, ze nam po odcitani z predchadzajucej rekurzie ostalo este nieco, co musime vyplatit
@@ -92,16 +94,55 @@ void payout(int * usedCoins, int remainingSum, int ignoreIndex){
         }
         else{
         //uz neexistuju ziadne mince mensej nominalnej hodnoty
+            delete(usedCoins);
             return;
         }
     }
     //pripad, ze po odcitani nam vznikla zaporna suma ... musime skoncit, lebo nemame spravne riesenie
     else{
+        delete(usedCoins);
         return;
+    }
+}
+void output() {
+    if(foundSolution){
+        returnCoinsAmount = 0;
+        for (int i = 0; i < CoinsTypeCount; ++i) {
+            returnCoinsAmount += returnCount[i];
+        }
+        cout << "Pocet pouzitych minci: " << returnCoinsAmount << endl;
+        for (int i = 0; i < CoinsTypeCount; ++i) {
+            int index = getIndex(returnValue[i]);
+            cout << "Minca " << returnValue[i] << ": " << returnCount[index] << endl;
+        }
+    }
+    else{
+        cout << "Suma sa neda zaplatit";
     }
 }
 
 int main() {
+    /*
+     * POSTUP RIESENIA:
+     * nacitanie vstupu
+     *      nominalna hodnota minci do pola coinValue
+     *      pocet minci pre danu nominalnu hodnotu do pola coinCount
+     * jednoduchym odhadom je vidiet, ze generovanie vsetkych podmnozin by dalo exponencionalnu zlozitost
+     * rozhodol som sa preto pouzit niekolko optimalizacii:
+     *      1. usporiadanie nominalnych hodnot mincii - quicksort O(n*logn)
+     *      2. ukoncenie vypoctu, ak dosiahnem sumu, ktoru mam vyplatit
+     *      3. ukoncenie vypoctu, ak presiahnem sumu, ktoru mam vyplatit
+     * tato zlozitost je este stale exponencialna vo worst-case (ba este horsia, kvoli triedeniu - to sa ale pre privysoke N strati)
+     *          ale v priemere by mala dosahovat prijemne vysledky
+     *
+     *  smernik returnValue odkazuje na polia, ktore si pamataju povodne zadane poradie mincii
+     *
+     *  returnCount nastavujem na MAXINT aby som predisiel chybnym prvym scitaniam - hned po prvom dosiahnuti vysledku uz nabera normalne hodnoty
+     *
+     *  returnCoinsAmount
+     */
+
+
     //nacitame pocet minci a sumu na zaplatenie
     cin >> CoinsTypeCount >> Amount;
     //naalokujeme miesto pre polia
@@ -116,28 +157,20 @@ int main() {
         returnCount[i] = INT8_MAX;
         returnValue[i] = coinValue[i];
     }
+
     //zotriedime mince podla nominalnej hodnoty
     quicksort(0, CoinsTypeCount);
+
     //vyplatime sumu
+    //pred spustenim rekurzie vytvorim pole, ktore si budem podavat v rekurzii
     int * actualCount = new int[CoinsTypeCount];
-
     for (int i = 0; i < CoinsTypeCount; ++i) actualCount[i] = 0;
-
     payout(actualCount, Amount, 0);
-    if(foundSolution){
-        int returnCoinsAmount = 0;
-        for (int i = 0; i < CoinsTypeCount; ++i) {
-            returnCoinsAmount += returnCount[i];
-        }
-        cout << "Pocet pouzitych minci: " << returnCoinsAmount << endl;
-        for (int i = 0; i < CoinsTypeCount; ++i) {
-            int index = getIndex(returnValue[i]);
-            cout << "Minca " << returnValue[i] << ": " << returnCount[index] << endl;
-        }
-    }
-    else{
-        cout << "Suma sa neda zaplatit";
-    }
-
+    delete(actualCount); // toto mazanie dokonca ani nemusi byt potrebne, kedze mazem aj v payout
+    //vypis
+    output();
+    //mazanie dalej nepouzivanych poli
+    delete(coinValue, coinCount, returnCount, returnValue);
     return 0;
 }
+
